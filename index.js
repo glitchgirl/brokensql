d41e60d5a29cc80c7ec323fdab579058ae351439
ri-ca4 <riley.de12@gmail.com>
2022-05-24 20:59:23 -0400

    added to index.js, changed file structure, added method to form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var mysql = require('mysql');
-
+const bodyParser = require('body-parser');
 var app = express();
 
 var connection = mysql.createConnection({
@@ -12,13 +12,36 @@ var connection = mysql.createConnection({
     password : ''
   });
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(express.json());
+
 connection.connect();
- 
-connection.query('select * from customers', function (error, results, fields) {
+connection.query('SELECT * FROM `users`', function (error, results, fields) {
   if (error) throw error;
-  console.log('The solution is: ', results);
+  console.log(results);
 });
 
-connection.end();
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.post('/user', (req, res) => {
+  let name = req.body.name;
+  let username = req.body.username;
+  let email = req.body.email;
+
+  connection.query('INSERT INTO `users` (`name`,`username`, `email`) VALUES (?,?,?)', [name, username, email] ,(err) => {
+      if (err){ 
+          console.log(err)
+      }else{
+          console.log('record inserted');
+          res.redirect('/');
+      }
+  });
+});
+
+
+app.listen(5000);
 
 module.exports = app;
